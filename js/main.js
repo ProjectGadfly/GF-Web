e9b49945af2d666fac2b94bf9832348e7705ac0e
An Hoang <An@Ans-MacBook-Pro.local>
2017-04-23 12:14:32 -0500

    4.23.2017

--- js/main.js
+++ js/main.js
@@ -156,7 +156,7 @@ $('#address-form').on('submit', function(e) {
     replaceTemplate(submit);
     console.log("3");
 });
-){
+{
     if(submit === true){
     $('div.front-page').remove();
     $('div.hidden').removeClass('hidden');
